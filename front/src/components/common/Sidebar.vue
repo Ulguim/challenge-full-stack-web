4d72a3ee7953d2fe7a6b1088b427e02a37c2d1fb
Ulguim <williamulguim2014@gmail.com>
2025-06-28 17:23:22 -0300

    :tada: feature/front - Initial configuration

---
+++ front/src/components/common/Sidebar.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  import { ref } from 'vue';
+
+  const drawer = ref(true)
+</script>
+
+<template>
+  <v-navigation-drawer v-model="drawer" app permanent>
+    <v-list>
+      <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" />
+      <v-list-item prepend-icon="mdi-account" title="Usuários" />
+      <v-list-item prepend-icon="mdi-cog" title="Configurações" />
+    </v-list>
+  </v-navigation-drawer>
+</template>
