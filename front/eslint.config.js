07de3b8d6d303dc60ff7b29c73e34270db818aa5
Ulguim <williamulguim2014@gmail.com>
2025-07-01 14:40:08 -0300

    feat(frontend/student): Changes eslint ruleset

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,3 +1,8 @@
 import vuetify from 'eslint-config-vuetify'
 
-export default vuetify()
+export default vuetify({
+  rules: {
+    'vue/script-indent': 'off',
+    '@stylistic/semi': 'off',
+  },
+})
